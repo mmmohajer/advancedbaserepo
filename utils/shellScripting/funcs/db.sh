cd18b69198b2edce15a9d1bf4c859f48e6ab35af
mmmohajer70 <mmmohajer70@gmail.com>
2025-09-25 13:32:54 -0400

    Open AI pricing fixed

--- utils/shellScripting/funcs/db.sh
+++ utils/shellScripting/funcs/db.sh
@@ -2,14 +2,14 @@ makeBackupOfDb() {
     echo "$POSTGRES_PASSWORD"
     local backupFileName=$(readData "What is backup filename  (without.sql)?")
     local addr="db_backups/$backupFileName.sql"
-    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 pg_dump devdb -U devuser > $addr
+    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 pg_dump $POSTGRES_DB -U $POSTGRES_USER > $addr
     echo "Backup file is ready"
 }
 
 restoreDb() {
     local restoreFileName=$(readData "What is restore filename (without.sql)?")
     local addr="db_backups/$restoreFileName.sql"
-    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 psql -U devuser -d devdb -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 psql -U devuser -d devdb -f $addr
+    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+    docker exec -e PGPASSWORD=$POSTGRES_PASSWORD app-db-1 psql -U $POSTGRES_USER -d $POSTGRES_DB -f $addr
     echo "DB succsessfully restored"
 }
